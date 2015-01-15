a48885ea67e99971cb9b1d1ef53f862c6b4d24a2
Martin Horak <horak@styrax.info>
2015-01-15 15:18:24 +0100

    Added load script

--- modeltranslation_sync/management/commands/load_trans.py
+++ modeltranslation_sync/management/commands/load_trans.py
@@ -1,22 +1,45 @@
 # coding: utf-8
 from __future__ import unicode_literals
+from os.path import join, isdir
+from django.conf import settings
 
-from optparse import make_option
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import NoArgsCommand, CommandError
+from django.db.models.loading import get_model
+from babel.messages.pofile import read_po
 
 
 class Command(NoArgsCommand):
 
-    option_list = NoArgsCommand.option_list + (
-        make_option(
-            '--dry-run',
-            action='store_true',
-            dest='dry_run',
-            default=False,
-            help='Do not actually send signals (and all connected stuff).'
-        ),
-    )
-
     def handle(self, *args, **options):
-        if not options['dry_run']:
-            pass
+
+        if not hasattr(settings, 'MODELTRANSLAION_LOCALE_PATH'):
+            raise CommandError("Settings has no attribute 'LOCALE_MODEL_TRANS'")
+
+        if not hasattr(settings, 'MODELTRANSLAION_PO_FILE'):
+            filename_po = "modeltranslation.po"
+        else:
+            filename_po = settings.MODELTRANSLAION_PO_FILE
+            if not filename_po.endswith(".po"):
+                filename_po += '.po'
+
+        locale_path = settings.MODELTRANSLAION_LOCALE_PATH
+        if not isdir(locale_path):
+            raise CommandError("Locale directory does not exists.")
+
+        for lang in [l[0] for l in list(settings.LANGUAGES)]:
+
+            lang_path = join(locale_path, lang)
+            if not isdir(lang_path):
+                raise CommandError("Language directory does not exists.")
+            f = open(join(lang_path, "LC_MESSAGES", filename_po), "r")
+            catalog = read_po(f)
+            f.close()
+
+            for message in catalog:
+                if message.id:
+                    [app, class_name, pk, field] = message.id.split('.')
+                    model = get_model(app, class_name)
+                    obj = model.objects.get(pk=pk)
+                    tr_field = "%s_%s" % (field, lang)
+                    setattr(obj, tr_field, message.string)
+                    obj.save()
