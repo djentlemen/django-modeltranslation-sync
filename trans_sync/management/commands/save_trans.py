91a9b86a0475594aac5208f7fc2cd1ce2c09bf91
Martin Horak <horak@styrax.info>
2015-01-15 13:27:49 +0100

    Fixed imports

--- trans_sync/management/commands/save_trans.py
+++ trans_sync/management/commands/save_trans.py
@@ -1,35 +1,22 @@
 # coding: utf-8
 from __future__ import unicode_literals
-import os
+from os import mkdir
 from os.path import join, isdir
-from optparse import make_option
+
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
 from modeltranslation.translator import translator
-
 from babel.messages.catalog import Catalog
 from babel.messages.pofile import write_po
 
 
 class Command(NoArgsCommand):
 
-    option_list = NoArgsCommand.option_list + (
-        make_option(
-            '--dry-run',
-            action='store_true',
-            dest='dry_run',
-            default=False,
-            help='Do not actually save files.'
-        ),
-    )
-
     def handle(self, *args, **options):
-        if not options['dry_run']:
-            pass
 
         locale_path = settings.LOCALE_MODEL_TRANS
         if not isdir(locale_path):
-            os.mkdir(locale_path)
+            mkdir(locale_path)
 
         for lang in [l[0] for l in list(settings.LANGUAGES)]:
 
@@ -46,9 +33,9 @@ class Command(NoArgsCommand):
                         catalog.add(id=msgid, string=msgstr)
 
             # write catalog to file
-            lang_path = os.path.join(locale_path, lang)
+            lang_path = join(locale_path, lang)
             if not isdir(lang_path):
-                os.mkdir(lang_path)
+                mkdir(lang_path)
             f = open(join(lang_path, "LC_MESSAGES", "modeltranslation.po"), "w")
             write_po(f, catalog)
             f.close()
